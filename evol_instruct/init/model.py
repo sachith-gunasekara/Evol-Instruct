0545ddcf95918a3c0255ce999880ed28d78a9914
Sachith Gunasekara <sachith@surge.global>
2024-06-16 00:44:03 +0530

    Add scripts to the project

--- evol_instruct/init/model.py
+++ evol_instruct/init/model.py
@@ -14,10 +14,14 @@ generator_model_path = hf_hub_download(
     config['model']['GeneratorModelGGMLFileName']
 )
 
+logger.info(f'Generator model {config['model']['GeneratorModel']} downloaded and is available at {generator_model_path}')
+
 # Model used for evaluating evolved instructions
 evaluator_model_ggml_path = hf_hub_download(
     config['model']['EvaluatorModel'],
     config['model']['EvaluatorModelGGMLFileName']
 )
+
+logger.info(f'Evaluator model {config["model"]["EvaluatorModel"]} downloaded and is available at {evaluator_model_ggml_path}')
 
 evaluator_model_gguf_path = os.path.join(os.path.dirname(evaluator_model_ggml_path), config['model']['EvaluatorModelGGUFFileName'])
---
+++ evol_instruct/scripts/prepare_generator_model.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone https://github.com/cmp-nct/ggllm.cpp
